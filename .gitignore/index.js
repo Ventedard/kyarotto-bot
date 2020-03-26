db6a78063483fd33ca11fd22e884553ebd5627cc
teo4420 <62711136+teo4420@users.noreply.github.com>
2020-03-26 18:05:05 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "c!";
 
-client.login("NjkyNzA3Nzk2Mjg1Nzg0MTA0.XnyfGA.Lo4btf98PGw0l5OICnGUpOIZmWQ");
+client.login(process.env.TOKEN);
 
 client.on("message", message =>{
     if(!message.guild) return
