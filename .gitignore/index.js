ae3bdff58343b819cff1685638813a0fe63a459b
teo4420 <62711136+teo4420@users.noreply.github.com>
2020-05-03 14:17:57 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -19,7 +19,7 @@ global.servers = {};
 client.on("message", message =>{
     if(!message.guild) return
     
-        client.user.setActivity("❤🐇Kyarotto🐇❤", { type: 'WATCHING'});
+        client.user.setActivity("Ventedard is the best dev ❤", { type: 'WATCHING'});
         client.user.setStatus('online')
 
     
